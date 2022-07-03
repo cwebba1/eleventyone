0c1049e6434a318a4876fbf37f992e3060e09074
Craig Webb <cwebba1@gmail.com>
2022-07-03 14:10:27 -0400

    2nd commit

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+const { DateTime } = require("luxon");
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("./app/css");
+	//eleventyConfig.addWatchTarget("app/css");
+    eleventyConfig.addPassthroughCopy("./app/assets");
+	//eleventyConfig.addWatchTarget("app/assets");
+  // eleventyConfig Format date
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+  //change resource.md to write to /resource.html
+  eleventyConfig.addGlobalData("permalink", () => {
+    return (data) => `${data.page.filePathStem}.${data.page.outputFileExtension}`;
+  });
+    // Return your Object options:
+    return {
+      dir: {
+        input: "app",
+        output: "dist"
+      }
+    };
+  }
---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+Read-Me
+
+#READ ME
+
+This is a prototype tutorial output based on Kevin Powell's YouTube video "urn static HTML/CSS into a blog with CMS using the JAMStack". 
+
+The tutorial link is at <https://www.youtube.com/watch?v=4wD00RT6d-g&t=1209s>.
+
+Much Thanks to Kevin Powell.
